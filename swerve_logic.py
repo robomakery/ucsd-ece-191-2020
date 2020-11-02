77085adaade4d9809f4a60b6f9d5f3857b73b187
aateek <72474026+aateek@users.noreply.github.com>
2020-11-02 15:44:48 -0800

    Update swerve_logic.py
    Changed the angles outputted to be based on 90 degree point that way can easily define clockwise as positive angle and counter-clockwise as negative angles

--- swerve_logic.py
+++ swerve_logic.py
@@ -46,61 +46,83 @@ class swerve_logic(object):
     #call the wheel object class
     whl = wheel()
 
-def rad_to_deg(self, radians):
-    #converts radian to degrees for easier calculations later
+    def rad_to_deg(self, radians):
+        #converts radian to degrees for easier calculations later
 
-    degrees = radians * 180 / math.pi
-    return degrees
+        degrees = radians * 180 / math.pi
+        return degrees
 
-def __init__(self, width, length):
+    def __init__(self, width, length):
 
-  #initialize these conditions when initializing the program
-  #width = wheelbase, length = wheel length
+      #initialize these conditions when initializing the program
+      #width = wheelbase, length = wheel length
 
-  self._R = math.sqrt(pow(width, 2) + pow(length, 2))
-  self._width = width
-  self._length = length
+      self._R = math.sqrt(pow(width, 2) + pow(length, 2))
+      self._width = width
+      self._length = length
 
-  #fin_speed_fr = 0, fin_speed_fl = 0, fin_speed_rl = 0, fin_speed_rr = 0, max_speed = 0
-  #fin_ang_fr = 0, fin_ang_fl = 0, fin_ang_rl = 0, fin_ang_rr = 0
+      #fin_speed_fr = 0, fin_speed_fl = 0, fin_speed_rl = 0, fin_speed_rr = 0, max_speed = 0
+      #fin_ang_fr = 0, fin_ang_fl = 0, fin_ang_rl = 0, fin_ang_rr = 0
 
-def calcWheelVect(self, x, y, ang_speed):
-#x and y of the wheel position vector and ang_speed	
-	#math needed to calculate the vectors in the drive in Resources defined in PDF and swerveMath
-	#calculate vectors needed to properly position wheels 
-    self.A = x - ang_speed * (self._length / self._R)
-    self.B = x + ang_speed * (self._length / self._R)
-    self.C = y - ang_speed * (self._width / self._R)
-    self.D = y + ang_speed * (self._width / self._R)
+    def calcWheelVect(self, x, y, ang_speed):
+        #x and y of the wheel position vector and ang_speed	all should be given from controller input (Controller hould have a max and min we need to normalize to be between -1 and 1)
+    	#math needed to calculate the vectors in the drive in Resources defined in PDF and swerveMath
+    	#calculate vectors needed to properly position wheels 
 
-	#calculates the wheel speeds needed based on the previously defined vectors
-    self.fin_speed_fr = math.sqrt(pow(self.B, 2) + pow(self.C, 2))
-    self.fin_speed_fl = math.sqrt(pow(self.B, 2) + pow(self.D, 2))
-    self.fin_speed_rl = math.sqrt(pow(self.A, 2) + pow(self.D, 2))
-    self.fin_speed_rr = math.sqrt(pow(self.A, 2) + pow(self.C, 2))
-    self.max_speed = max(self.fin_speed_fr, self.fin_speed_fl, self.fin_speed_rl, self.fin_speed_rr)
+        self.A = x - ang_speed * (self._length / self._R)
+        self.B = x + ang_speed * (self._length / self._R)
+        self.C = y - ang_speed * (self._width / self._R)
+        self.D = y + ang_speed * (self._width / self._R)
 
-	#normalize speed based on fastest wheel to scale vector down if needed
-    if self.max_speed > 1:
-        self.fin_speed_fr /= self.max_speed
-        self.fin_speed_fl /= self.max_speed
-        self.fin_speed_rl /= self.max_speed
-        self.fin_speed_rr /= self.max_speed
-         
-	#find the angles to position the wheels at dependent on the vectors calculated earlier  
-    self.fin_ang_fr = self.radians_to_degrees(math.atan2(self.B, self.C))
-    self.fin_ang_fl = self.radians_to_degrees(math.atan2(self.B, self.D))
-    self.fin_ang_rl = self.radians_to_degrees(math.atan2(self.A, self.D))
-    self.fin_ang_rr = self.radians_to_degrees(math.atan2(self.A, self.C))
+    	#calculates the wheel speeds needed based on the previously defined vectors
+        self.fin_speed_fr = math.sqrt(pow(self.B, 2) + pow(self.C, 2))
+        self.fin_speed_fl = math.sqrt(pow(self.B, 2) + pow(self.D, 2))
+        self.fin_speed_rl = math.sqrt(pow(self.A, 2) + pow(self.D, 2))
+        self.fin_speed_rr = math.sqrt(pow(self.A, 2) + pow(self.C, 2))
+        self.max_speed = max(self.fin_speed_fr, self.fin_speed_fl, self.fin_speed_rl, self.fin_speed_rr)
 
-	#set the wheel speed to the speed normalized and found previously
-    self.whl.speed_fr = self.fin_speed_fr
-    self.whl.speed_fl = self.fin_speed_fl
-    self.whl.speed_rl = self.fin_speed_rl
-    self.whl.speed_rr = self.fin_speed_rr
+    	#normalize speed based on fastest wheel to scale vector down if needed
+        if self.max_speed > 1:
+            self.fin_speed_fr /= self.max_speed
+            self.fin_speed_fl /= self.max_speed
+            self.fin_speed_rl /= self.max_speed
+            self.fin_speed_rr /= self.max_speed
+             
+    	#find the angles to position the wheels at dependent on the vectors calculated earlier if not over max speed
+        self.fin_ang_fr = self.rad_to_deg(math.atan2(self.B, self.C))
+        self.fin_ang_fl = self.rad_to_deg(math.atan2(self.B, self.D))
+        self.fin_ang_rl = self.rad_to_deg(math.atan2(self.A, self.D))
+        self.fin_ang_rr = self.rad_to_deg(math.atan2(self.A, self.C))
 
-	#make sure proper angle is found because atan used and send to wheel object
-    self.whl.angle_fr = 360 - (self.fin_ang_fr + 180)
-    self.whl.angle_fl = 360 - (self.fin_ang_fl + 180)
-    self.whl.angle_rl = 360 - (self.fin_ang_rl + 180)
-    self.whl.angle_rr = 360 - (self.fin_ang_rr + 180)
+    	#set the wheel speed to the speed normalized and found previously
+        self.whl.speed_fr = self.fin_speed_fr
+        self.whl.speed_fl = self.fin_speed_fl
+        self.whl.speed_rl = self.fin_speed_rl
+        self.whl.speed_rr = self.fin_speed_rr
+
+    	#make sure proper angle is found given from 90 deg clockwise to new wheel location
+        self.whl.angle_fr = self.fin_ang_fr
+        self.whl.angle_fl = self.fin_ang_fl
+        self.whl.angle_rl = self.fin_ang_rl
+        self.whl.angle_rr = self.fin_ang_rr
+
+def main():
+    test = swerve_logic(1, 1)
+    test.calcWheelVect(1,-1,1)
+    print(test._R)
+    print('\n')
+
+    print(test.whl.speed_fr)
+    print(test.whl.speed_fl)
+    print(test.whl.speed_rl)
+    print(test.whl.speed_rr)
+
+    print('\n')
+
+    print(test.whl.angle_fr)
+    print(test.whl.angle_fl)
+    print(test.whl.angle_rl)
+    print(test.whl.angle_rr)
+
+if __name__ == "__main__":
+    main()
