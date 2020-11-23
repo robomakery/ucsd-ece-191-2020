4c4a500ab0b0f11da169d2fa40517e010e3e34c7
Dylan Vaughn <dylan@ordinaryexperts.com>
2020-11-23 13:47:51 -0800

    only load setup.bash if it exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 
 # setup ros environment
 source "/opt/ros/melodic/setup.bash"
-source "/code/robot_ws/devel/setup.bash"
+if [ -f "/code/robot_ws/devel/setup.bash" ]; then
+    source "/code/robot_ws/devel/setup.bash"
+fi
 exec "$@"
 
